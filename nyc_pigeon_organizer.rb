4d25966637d32829cfe727b698ce234b1bb770ee
Juan Fernandez <jcfernan@live.com>
2020-05-01 17:04:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,19 @@
 def nyc_pigeon_organizer(data)
-  pigeon_by_name ={}
+  pigeon_by_name = {}
   data.each do |key, value|
-    value.each do |describe, array|
-      array.each do |name|
-        if pigeon_by_name.has_key?(name)
-          if pigeon_by_name[name].has_key?(key)
-            pigeon_by_name[name][key] << describe.to_s
-          else
-            pigeon_by_name[name][key] = [describe.to_s]
-          end
-        else
-          pigeon_by_name[name] = {key => [describe.to_s]}
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !pigeon_by_name[name]
+          pigeon_by_name[name] = {}
         end
+
+        if !pigeon_by_name[name][key]
+          pigeon_by_name[name][key] = []
+        end
+
+        pigeon_by_name[name][key] << pigeon_by_name.to_s
+
       end
     end
   end
